cac132dcbac5f7a3813023753c87b9f27f1d26b4
Muhammad Al Biruni <50983566+birunidev@users.noreply.github.com>
2020-08-23 14:45:20 +0700

    Update DoMore.tsx

--- src/components/UI/DoMore.tsx
+++ src/components/UI/DoMore.tsx
@@ -12,7 +12,7 @@ export default function DoMore({ title }) {
       data-sal-duration="1000"
     >
       <p className="h2 mb-5 font-weight-bold text-center text-gagalin text-white">
-        We do more than just <span className="text-warning">{title}:</span>{" "}
+        We do more than just <span className="text-warning">{title}.</span>{" "}
         <br /> Explore our different digital marketing services.
       </p>
       <Link to="/services" className="btn btn-warning btn-lg font-weight-bold">
